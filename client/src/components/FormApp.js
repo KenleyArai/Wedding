c9d8aa74eac6d84ce91b428355ea25197c985e8f
Kenley Arai <me@kenleyarai.com>
2017-09-17 19:11:33 -0700

    improved ui

--- client/src/components/FormApp.js
+++ client/src/components/FormApp.js
@@ -48,7 +48,7 @@ export default class FormApp extends React.Component {
     componentDidMount() {
         this.timerID = setInterval(
           () => this.tick(),
-            5000
+            250
         );
       }
 
